1b6fffbe9beaf120e5e47f79f1b880b8f541e383
jaysonmulwa <jayson.mulwa@gmail.com>
2022-07-22 23:01:02 +0300

    chore: knapsack container vol problem

--- algorithms/greedy-algorithm/knapsack.js
+++ algorithms/greedy-algorithm/knapsack.js
@@ -24,3 +24,27 @@ let items = [
 ];
 
 console.log(knapsack(items, 14));
+
+
+//!Leetcode Problem 11 - Container With Most Water 
+//Bruteforce is O(n*n)
+//Pointers method is O(n) - solution below 
+ var maxArea = function(height) {
+    let maxArea= 0;
+    let left= 0;
+    let right = height.length-1;
+     // we will keep moving till we cross the right
+     while(left<right){
+         let width= right-left;
+         let minHeight= Math.min(height[left],height[right]);
+         maxArea = Math.max(maxArea,(width*minHeight));
+         
+         // the logic is we will keep going closer to the max height either from left or right
+         if(height[left]<height[right]){
+             left++;
+         }else{
+             right--;
+         }
+     }
+     return maxArea;
+ };
