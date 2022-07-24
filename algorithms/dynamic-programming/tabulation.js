8720b3740596b017e382ad397f13821398c00d3f
jaysonmulwa <jayson.mulwa@gmail.com>
2022-07-24 21:58:34 +0300

    feat: to be continued

--- algorithms/dynamic-programming/tabulation.js
+++ algorithms/dynamic-programming/tabulation.js
@@ -37,8 +37,8 @@ const gridTraveler = (m, n) => {
   return table;
 };
 
-//Time O(m * n)
-//Space O(m)
+//--Time O(m * n)
+//--Space O(m)
 const canSum = (m, arr) => {
     const table = Array(m + 1).fill(false);
     table[0] = true;
@@ -54,7 +54,43 @@ const canSum = (m, arr) => {
         }
     }
     return table;
- 
 }
 
-console.log(canSum(7, [3, 4, 5]));
+
+
+/*const howSum = (m, arr) => {
+    const table = Array(m + 1).fill(null);
+    table[0] = [];
+    for (let i = 0; i <= table.length; i++){
+        let curr = table[i];
+        for (let j = 0; j < arr.length; j++ ) {
+            let num = arr[j];
+            let nextIndex = i + num;
+
+                if ((nextIndex < table.length)) {
+                    if (table[nextIndex] == null) {
+                        table[nextIndex] = [];
+                    }
+                    table[nextIndex].push(num);
+                }
+            
+        }
+        sum = 0;
+        if (table[m] !== null) {
+            //console.log(table[m]);
+            table[m].forEach(element => {
+                sum += element;
+            });
+            
+            if (sum == m) {
+               // return table[m];
+            }
+        }
+
+        console.log(table);
+    }
+
+    return [];
+}
+
+console.log(howSum(7, [3, 4, 5]));*/
