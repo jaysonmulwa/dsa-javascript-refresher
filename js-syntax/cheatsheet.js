99e87444cb899f1ef803e475eaf11e708007f044
jaysonmulwa <jayson.mulwa@gmail.com>
2022-07-26 16:30:00 +0300

    feat: added new methods

--- js-syntax/cheatsheet.js
+++ js-syntax/cheatsheet.js
@@ -1,5 +1,5 @@
 /**
- * ES6 Cheat Sheet
+ * JS/ES6 Cheat Sheet
  */
 
 /*
@@ -69,3 +69,72 @@ arr.find((element) =>{
 arr.findIndex((element) =>{
     return element > 2;
 });
+
+
+/**
+ * String methods
+ */
+
+//1. charAt -- returns the character at the specified index
+word.charAt(2);
+
+//2. charCodeAt -- returns the unicode value of the character at the specified index
+word.charCodeAt(2);
+
+
+//3. concat -- returns a new string with the current string concatenated with the given string
+word.concat("hello");
+
+//4. indexOf -- returns the index of the first occurrence of the given substring //!important
+word.indexOf("2");
+
+//5. lastIndexOf -- returns the index of the last occurrence of the given substring
+word.lastIndexOf("2");
+
+//6.To lowercase -- returns a new string with all characters converted to lowercase
+word.toLowerCase();
+
+//7.To uppercase -- returns a new string with all characters converted to uppercase
+word.toUpperCase();
+
+//8.To integer -- returns the string as an integer
+word.toInt();
+
+
+/**
+ * Number methods
+ */
+
+//1. toString -- returns a string representation of the number
+Number.toString();
+
+//2. toFixed -- returns a string representation of the number with the specified number of decimals
+Number.toFixed(2);
+
+/**
+ * Math methods
+ */
+
+//1. random -- returns a random number between 0 and 1
+Math.random();
+
+//2. floor -- returns the largest integer less than or equal to a number
+Math.floor(2.9);
+
+//3. ceil -- returns the smallest integer greater than or equal to a number
+Math.ceil(2.1);
+
+//4. round -- returns the nearest integer to a number
+Math.round(2.4);
+
+//5. abs -- returns the absolute value of a number
+Math.abs(-2);
+
+//6. pow -- returns the value of a number to the power of another number
+Math.pow(2, 3);
+
+//7. Min -- returns the smallest number in the array
+Math.min([1, 2, 3]);
+
+//8. Max -- returns the largest number in the array
+Math.max([1, 2, 3]);
