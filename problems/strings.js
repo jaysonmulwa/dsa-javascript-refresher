fffc5c68308542edab9bd23096db40ca456788f6
jaysonmulwa <jayson.mulwa@gmail.com>
2022-07-29 20:15:55 +0300

    feat: longest substring without repeating characters

--- problems/strings.js
+++ problems/strings.js
@@ -6,3 +6,23 @@ const areAnagram = (str1, str2) => str1.toLowerCase().split('').sort().join('') 
 
 //!Longest substring without repeating characters
 //Sliding window
+const longestSubstring = (word) => {
+    let i = 0;
+    let j = 0;
+    let max = 0;
+    let arr = word.split(""); 
+    let map = new Map();
+    while (j < arr.length) {
+        if (!map.has(arr[j])){
+            map.set(arr[j], 1);
+            j++;
+            max = Math.max(map.size, max);
+        } else {
+            map.delete(arr[i]);
+            i++;
+        }
+    }
+    return max;
+}
+
+console.log(longestSubstring("abba"));
