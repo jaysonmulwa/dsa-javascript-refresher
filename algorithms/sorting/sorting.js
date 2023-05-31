e83208c778e8ff1c351936659a4de123c6e8a798
Jayson Mulwa <jayson.mulwa@gmail.com>
2023-05-31 19:42:57 +0300

    feat: updated sorting notes

--- algorithms/sorting/sorting.js
+++ algorithms/sorting/sorting.js
@@ -4,6 +4,39 @@
  *
  * Insertion Sort = In-place sorting algorithm. O(n * n) - Worst Case; Best case is O(n)
  * * Maintains 2 sublists, a sorted and unsorted list. element from unsorted list is added onto the correct place in sorted list.
+ * 
+ * * Common sorting algorithms
+ *! 1. Quicksort:
+ * - Quicksort is a divide-and-conquer algorithm that selects a pivot element and partitions the array around it. 
+ * - It has an average time complexity of O(n log n) and is often faster than other sorting algorithms in practice. 
+ * - However, its worst-case time complexity is O(n^2) if the pivot selection is inefficient.
+ * 
+ *! 2. Mergesort: 
+ * - Mergesort is a divide-and-conquer algorithm that recursively divides the array into smaller sub-arrays, sorts them, and then merges them to obtain the final sorted array.
+ * - It has a consistent time complexity of O(n log n) in all cases but requires additional memory for merging, resulting in a space complexity of O(n).
+ * 
+ *! 3. Insertion Sort: 
+ * - Insertion Sort is an efficient algorithm for small input sizes or partially sorted arrays. 
+ * - It iterates through the array, comparing each element with the preceding elements and inserting it into the correct position. 
+ * - Its time complexity is O(n^2), but it performs well for small inputs.
+ * 
+ *! 4. Selection Sort: 
+ * - Selection Sort repeatedly finds the minimum element from the unsorted part of the array and swaps it with the element in the correct position. 
+ * - It has a time complexity of O(n^2) and is not suitable for large arrays due to its inefficient nature.
+ * 
+ *! 5. Bubble Sort:
+ * - Bubble Sort compares adjacent elements and swaps them if they are in the wrong order, gradually moving the larger elements towards the end of the array. 
+ * - It has a time complexity of O(n^2) and is primarily used for educational purposes or small input sizes due to its simplicity.
+ * 
+ *! 6. Heap Sort:
+ * - Heap Sort uses a binary heap data structure to sort elements.
+ * - It first builds a max-heap from the array and repeatedly extracts the maximum element, maintaining the heap property.
+ * - It has a time complexity of O(n log n) and performs in-place sorting.
+ * 
+ *! 7. Radix Sort:
+ * - Radix Sort sorts numbers by processing individual digits, starting from the least significant digit to the most significant digit.
+ * - It has a time complexity of O(d * (n + k)), where d is the number of digits and k is the range of possible digit values.
+ * 
  */
 
 class Sort {
