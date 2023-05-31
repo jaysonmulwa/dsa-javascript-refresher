0f2fec26479428b2770ccd57189fbe4738c67fca
Jayson Mulwa <jayson.mulwa@gmail.com>
2023-05-31 16:51:31 +0300

    Update trees.js

--- data-structures/trees.js
+++ data-structures/trees.js
@@ -1,1 +1,13 @@
-
+/**
+ * A tree is a nonlinear data structure that is used to store data in a hierarchical manner. A tree is made up of a set of nodes connected by edges.
+ *
+ * Trees:
+ * 1. Binary Search Trees
+ * 2. Red-Black Trees
+ * 3. B-Trees (An advanceed data structure)
+ * 4. Augmented Trees [Interval trees, Order-statistic trees]
+ *
+ *
+ *
+ *
+*/
